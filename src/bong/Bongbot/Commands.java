da5f7782f01c9f42f798c6e94d2f1da41ee66301
Yong Dong <yongdong11467@gmail.com>
2019-01-21 14:32:58 -0500

    added league functions

--- src/bong/Bongbot/Commands.java
+++ src/bong/Bongbot/Commands.java
@@ -1,39 +1,5 @@
 package bong.Bongbot;
 
-//import net.dv8tion.jda.core.EmbedBuilder;
-//import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
-//import net.dv8tion.jda.core.hooks.ListenerAdapter;
-//
-//public class Commands extends ListenerAdapter {
-//
-//    public void recieveMsg(GuildMessageReceivedEvent event) {
-//        System.out.println(event.getMessage().getContentRaw() + "hello");
-//        String[] msgDiv = event.getMessage().getContentRaw().split(" ");
-//        if (msgDiv[0].equalsIgnoreCase(Bongbot.prefix)) {
-//
-//            EmbedBuilder embedBuilder = new EmbedBuilder();
-//            embedBuilder.setTitle("Bongbot info");
-//            embedBuilder.setDescription("Weeb bot");
-//            embedBuilder.addField("Creator", "Bong", false);
-//            embedBuilder.setColor(0x92f441);
-//
-//            event.getChannel().sendTyping().queue();
-//            event.getChannel().sendMessage(embedBuilder.build()).queue();
-//            embedBuilder.clear();
-//
-//            switch (msgDiv[1]) {
-//                case "bong":
-//                    event.getChannel().sendMessage("Challenger").queue();
-//                    break;
-//                case "nick":
-//                    break;
-//                default:
-//                    event.getChannel().sendMessage("You retard that's not a command").queue();
-//            }
-//        }
-//    }
-//}
-
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -58,6 +24,21 @@ public class Commands extends ListenerAdapter {
                     event.getChannel().sendTyping().queue();
                     event.getChannel().sendMessage(embedBuilder.build()).queue();
                     embedBuilder.clear();
+                    break;
+                case "playlist":
+                    switch (args[2]) {
+                        case "add":
+                            break;
+                        case "remove":
+                            break;
+                        case "show":
+                    }
+                    break;
+                case "help":
+                    break;
+                case "league":
+                    String base = "http://na.op.gg/summoner/userName=";
+                    event.getChannel().sendMessage(base + args[2]).queue();
                     break;
                 default:
                     event.getChannel().sendMessage("You retard that's not a command").queue();
